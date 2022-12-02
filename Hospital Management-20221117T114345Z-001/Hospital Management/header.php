898b5c145769b4ae1f7c0fbd4682c2b6b3b337b8
swap1007 <97354319+swap1007@users.noreply.github.com>
2022-12-02 11:20:35 +0530

    Add files via upload

---
+++ Hospital Management-20221117T114345Z-001/Hospital Management/header.php
@@ -0,0 +1,6 @@
+<html>
+<head>
+<link rel="shortcut icon" type="image/x-icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdRoMCJDW_zv-x3a8CnKvRGVONz67yJqoNcw&usqp=CAU" />
+<!-- <link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" /> -->
+</head>
+</html>
